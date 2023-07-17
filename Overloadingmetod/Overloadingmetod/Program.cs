240ac4be773f703e160df47e9e4d25aeb217ef18
Sharaf034 <sharafet@code.edu.az>
2023-07-17 23:42:06 +0400

    Add new file

---
+++ Overloadingmetod/Overloadingmetod/Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+
+//2----//Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin.
+
+
+//string name;
+//Console.Write("ShowText = ");
+//name = Console.ReadLine();
+//for (int i = 0; i <= name.Length - 1; i++)
+//{
+//    Console.Write(name[name.Length - i - 1]);
+//}
+//Console.ReadLine();
+
+
+
+
+
+
+//2------//Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin.
+
+
+//static string ShowText(string text)
+//{
+//    string result = "Sharaf";
+
+//    for (int i = text.Length - 1; i >= 0; i--)            //niye cavab cixmir?
+//    {
+//        result += text[i];
+//    }
+
+//    return result;
+//}
+
+
+
+//3----String qebul eden bir method yazirsiz. Hemin method qebul etdiyi stringin ilk herfini boyuk yazdirmalidir
+
+//static string FirstCharToCharArray(string input)
+//{
+//    if (string.IsNullOrEmpty(input))
+//    {
+//        return string.Empty;
+//    }
+//    var stringArray = input.ToCharArray();                      //netden goturdugum melumatlari birlesdirdim..
+//    if (char.IsLower(stringArray[0]))
+//    {
+//        stringArray[0] = char.ToUpper(stringArray[0]);
+//    }
+//    return new string(stringArray);
+
+//}
+
+
+
+
+//3---- String qebul eden bir method yazirsiz. Hemin method qebul etdiyi stringin ilk herfini boyuk yazdirmalidir
+
+//static void Main(string text)
+//{
+//    string str = "academy";                                      //netden goturdugum melumatlari birlesdirdim..
+
+//    if (str.Length == 0)
+//        System.Console.WriteLine("academy");
+////    else if (str.Length == 1)
+////        System.Console.WriteLine(char.ToUpper(str[0]));
+////    else
+////        System.Console.WriteLine(char.ToUpper(str[0]) + str.Substring(1));
+
+//}
+
+
+
+
+
---
+++ Repeatclasswork/Repeatclasswork/Program.cs
@@ -0,0 +1,167 @@
+﻿
+
+
+
+
+
+//static void GetFullData(string name,string surname,int age,string adress = "Ehmedli")
+//{
+//    string result = $"Name: {name}, Surname: {surname} , Age: {age}, Adress: {adress}";
+//    Console.WriteLine(result);
+//}
+
+//GetFullData("Sheref", "Tenzilli", 26);
+
+
+
+//static void GetNumsOfArray(int number, int num, params int[] arr)
+//{
+//    foreach (var item in arr)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+////int[] nuumbers = { 2, 3, 5, 6, 7 };
+
+//GetNumsOfArray(1, 2, 3, 4, 5);
+
+
+
+
+//int a = 5;
+//int b = a;
+//b = 100;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+
+
+
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+
+//Console.WriteLine(arr1[0]);
+//int[] arr2 = arr1;
+//arr2[0] = 100;
+
+//Console.WriteLine(arr2[0]);
+//Console.WriteLine(arr2[0]);
+
+
+
+
+//int a = 8;
+//Test(ref a);
+//Console.WriteLine(a);
+
+
+//static void Test(ref int a)
+//{
+//    a = 20;
+//    Console.WriteLine(a);
+//}
+
+
+
+//static void GetFullName(string name, string surname)
+//{
+//    Console.WriteLine(name+ "--" +surname);
+//}
+//GetFullName("Sharaf", "Tanzili");
+
+
+
+
+
+//StringBuilder stringBuilder = new StringBuilder("Salam");
+//stringBuilder.Append("-Azerbaycan");
+//Console.WriteLine(stringBuilder);
+
+
+
+//String name = new String("Salamlar");
+//string surname = "";
+//Console.WriteLine(name);
+
+
+
+//string name = "Samir";
+//Test(name);
+//Console.WriteLine(name);
+
+
+//static void Test(string name);
+//{
+//    name = "Xeyyam";
+//    Console.WriteLine(name);
+//}
+
+
+
+//string name = "Pervin";
+
+////Console.WriteLine(name.Length);
+////Console.WriteLine(name.ToLower());
+////Console.WriteLine(name.Trim());
+////Console.WriteLine(name.Replace("e", "a"));
+
+
+//	for (int i = 0; i < name.Length; i++)
+//{
+//    Console.WriteLine(name[i]);
+//}
+
+
+
+//string name = "Salam Azerbaycan";
+
+//var arr = name.Split("");
+//string result = arr.ToString();
+
+
+
+//string str1 = "Programming vdgshdhvdsjkgbskjgj";
+//string str2 = "Programming";
+
+//////Console.WriteLine(str1 + str2);
+
+//////string result = string.Concat(str1, str2);
+
+//////Console.WriteLine("Joined string: " +result);
+
+
+////if (str1==str2)
+////{
+////    Console.WriteLine(true);
+////}
+////else
+////{
+////    Console.WriteLine(false);
+////}
+
+////bool isEqual1 = str1.Equals(str2);
+////Console.WriteLine(isEqual1);
+
+//Console.WriteLine(str1.Substring(0,10) + "...");
+
+
+
+
+//string surname = "Filankesov";
+//Console.WriteLine(surname.Contains("z"));
+
+
+
+
+//string text = "salam";
+
+//ReplaceSpecificCharFromStr(text, 0, 't');
+
+
+//static void ReplaceSpecificCharFromStr(string str, int index, char replaceChar)
+//{
+//    var arr = str.ToCharArray();
+//    arr[index] = replaceChar;
+//    arr.ToString();
+//    Console.WriteLine(arr);
+//}
